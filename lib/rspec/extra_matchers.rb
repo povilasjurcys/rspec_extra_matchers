61750aeea521563aa174084a259871949b1fe74b
Povilas Jurcys <po.jurcys@gmail.com>
2023-07-18 09:09:10 +0300

    Initial commit

---
+++ lib/rspec/extra_matchers.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "extra_matchers/version"
+
+module Rspec
+  module ExtraMatchers
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
